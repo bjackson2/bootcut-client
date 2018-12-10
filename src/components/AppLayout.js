54ee53f8705576f42c80e0b913eb57d9a34bc8c1
Ben Jackson <bjackson2@gmail.com>
2018-12-09 19:26:23 -0500

    Adds start of app layout, logo image

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {Container, Image, Menu} from 'semantic-ui-react';
 
 export default ({children}) => (
   <div>
-    <h1>Bootcut!</h1>
-    <div>{children}</div>
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item as="a" header>
+          <Image size="mini" src="/om.png" style={{marginRight: '1.5em'}} />
+          Bootcut
+        </Menu.Item>
+        <Menu.Item as="a">Lobby</Menu.Item>
+      </Container>
+    </Menu>
+    <Container style={{marginTop: '7em'}}>{children}</Container>
   </div>
 );
